531dc437ae8fb649adbf5d5ac8792f7e3ac07be2
Allison Moore <amoore214@tamu.edu>
2023-09-27 22:42:20 -0400

    Iterate through Z. This is probably wrong.

--- REML_geoR.R
+++ REML_geoR.R
@@ -1,12 +1,11 @@
-# Load the GeoR package
+
 library(geoR)
+library(fields)
 set.seed(42) 
 
 
-# Assuming you have the following data
 n <- 100
 coords <- cbind(runif(n, 0, 2), runif(n, 0, 2)) # Random 2-D coordinates
-
 
 # Generate the Matern covariance matrix Sigma_grid
 alpha <- 1 / 0.75
@@ -16,17 +15,26 @@ D <- rdist(coords) # Distance matrix
 Sigma_grid <- Matern(D, alpha = alpha, nu = nu, phi = phi)
 
 # Generate the observed data 'z' using multivariate normal distribution
+z <- rmvnorm(n = n, mean = rep(0, n), sigma = Sigma_grid)
 
-z <- rmvnorm(n = n, mean = 0, sigma = Sigma_grid)
+# Fit separate Matern models for each column of 'z' using REML
+# Not sure if this even remotely something that should be done.
+variogram_fits <- lapply(1:ncol(z), function(col) {
+  likfit(
+    geodata = list(coords = coords, data = z[, col]), # Use each column of 'z'
+    ini.cov.pars = c(1, 1), 
+    fix.nugget = TRUE, # Assuming a fixed nugget
+    lik.method = "REML" # Use REML estimation
+  )
+})
 
+# Calculate the average values of the model parameters
+avg_params <- sapply(variogram_fits, function(fit) {
+  cov.pars <- fit$cov.pars
+  names(cov.pars) <- c("Sigma_sq", "Range")
+  cov.pars
+})
 
-# Fit the Matern model using REML
-variogram_fit <- likfit(
-  geodata = list(coords = coords, data = z[,1]), # Can't get this to run with more than one column for data...
-  ini.cov.pars = c(1, 1),
-  fix.nugget = TRUE, # Assuming a fixed nugget
-  lik.method = "REML" # Use REML estimation
-)
-
-# Print a summary of the REML estimates
-summary(variogram_fit)
+# Print the average values of the model parameters
+cat("Average Model Parameters:\n")
+print(rowMeans(avg_params))
