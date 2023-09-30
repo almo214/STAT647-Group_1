9b9372f38863d332e6f8fa7b7d705b5163fd351c
almo214 <111311631+almo214@users.noreply.github.com>
2023-09-30 14:30:32 -0400

    Update REML. Add set.seeds

--- HW2_Group1.R
+++ HW2_Group1.R
@@ -16,17 +16,18 @@
 library(fields)
 library(MASS)
 library(Matrix)
+library(geoR) 
 # library(RandomFields) # Library for Exponential damping no longer supported
 
-# source("REML_Group1.R") # Note: I commented this out on my Rmd file since the code won't run if I kept 
-# it here
+# Define all Functions ####
 
-### Simulations for Model 1 (exponential cov)
-## Generate data
-n  <- 100
 
-coords <- cbind(runif(n,0,2),runif(n,0,2)) # Over range [0, 2]
-D <- rdist(coords) # distance matrix
+# Define Exponential Damped covariance 
+exp_Damped_cov <- function(h, rho, phi) {
+  phi * exp(-h / rho) * cos(h)
+}
+
+
 
 ## Define GMLE function
 likelihood.exponential_GMLE <- function(cov.pars) {
@@ -57,45 +58,37 @@ likelihood.exponential_GMLE <- function(cov.pars) {
   return(temp4 / 2)
 }
 
-## Define REML function
-## UPDATE: The second term of "logpart" isn't accurate. Manually coding out the second part will be 
-## cumbersome, so prof. recommended us to find the REML package in R and put in correct parameters 
-## accordingly. Also, estimating the mean of Y(s) isn't necessary for REML, so that is why parameter 
-## "mu" is not present in the function
-likelihood.exponential_REML <- function(cov.pars) {
-  sigma <- cov.pars[1]
-  rho <- cov.pars[2]
-  
-  # Calculate the exponential covariance
-  cov <- (sigma^2) * exp(-D / rho^2)
-  
-  # Calculate Cholesky decomposition
-  temp <- chol(nearPD(cov)$mat)
-  
-  # Calculate log likelihood components
-  logpart <- 2 * sum(log(diag(temp))) + 2 * (sum(log(diag(t(coords) %*% temp %*% coords))))
-  # Appears the only difference between GMLE and REML is the second portion of logpart above.
-  
-  step1 <- forwardsolve(t(temp), t(z))
-  step2 <- backsolve(temp, step1)
-  exponentpart <- z %*% step2
-  
-  # Negative log-likelihood
-  temp4 <- logpart + exponentpart
-  
-  return(temp4 / 2)
-}
+
+
+
+
+
+
+### Simulations for Model 1 (exponential cov)
+## Generate data
+set.seed(42)
+n  <- 100
+
+coords <- cbind(runif(n,0,2),runif(n,0,2)) # Over range [0, 2]
+D <- rdist(coords) # distance matrix
+
+
+rho <- .75
+alpha <- 1 / rho
+nu <- 0.5
+phi <- 4.0
+
 
 ## Create a table for the parameter and mean estimates per simulation. Set the number of simulations, 
 ## M=50 reps
 ## (Note: Will eventually change M to 1000 once we get the code working properly for small sim size.)
 ## Method 1: GMLE
-sims1 <- matrix(NA, nrow=50, ncol=3) # UPDATE: Added a third column in the sims matrix that stores the 
+sims1 <- matrix(NA, nrow=1000, ncol=3) # UPDATE: Added a third column in the sims matrix that stores the 
 # estimates for the Y(s)_hat
 
 ## Obtain the estimates and populate the sims1 table
-for(i in c(1:50)){ # nrow(sims1)=M=50, 1<=i<=50 reps
-  
+for(i in c(1:1000)){ # nrow(sims1)=M=50, 1<=i<=50 reps
+  set.seed(i)
   Sigma_grid = Matern(D, alpha=1/.75,nu=0.5,phi=4.0) # Is this the correct thing to use for HW2? 
   # A: Not quite! It is correct that you want to use alpha=1/range=1/.75, but phi should be gamma^2=2^2=4
   # since it represents the *marginal variance* of the exponential process
@@ -106,7 +99,7 @@ for(i in c(1:50)){ # nrow(sims1)=M=50, 1<=i<=50 reps
  
   cov.pars<-c(1,1,1) # initial parameter values
   
-  out1 <- nlm(likelihood.exponential_GMLE, cov.pars, stepmax=5, print.level=2, gradtol=10^(-10))
+  out1 <- nlm(likelihood.exponential_GMLE, cov.pars, stepmax=2, print.level=0, gradtol=10^(-6))
 
   out2 <- (out1$est)**2
 
@@ -116,7 +109,7 @@ for(i in c(1:50)){ # nrow(sims1)=M=50, 1<=i<=50 reps
   ## reflection of the estimated mean [ideally, it should equal to approximately 5]. So we go back and 
   ## square root all the mean estimates in col 3 of sims1
   sims1[i,3]=sqrt(sims1[i,3])
-  
+  print(i)
 }
 
 ## Obtain distribution, mean, and sd for each param estimate of the above simulation
@@ -131,24 +124,40 @@ sd(sims1[,2]) # Standard deviation of rho
 sd(sims1[,3]) # Standard deviation of Y(s)
 
 ## Method 2: REML
-sims2 <- matrix(NA, nrow=50, ncol=2)
+sims2 <- matrix(NA, nrow=5, ncol=2)
 
-for(i in c(1:50)){ 
+for(i in c(1:5)){ 
   
-  Sigma_grid = Matern(D, alpha=1/.75,nu=0.5,phi=4.0) 
+  Sigma_grid <- Matern(D, alpha = alpha, nu = nu, phi = phi) # Generate the Matern covariance matrix, 
+  # Sigma_grid
   
-  z = rmvnorm(n = 1, mu = rep(5,n), Sigma = Sigma_grid)
- 
-  cov.pars<-c(1,1)
+  z <- rmvnorm(n = n, mean = rep(0, n), sigma = Sigma_grid) # Using Exponential cov. function
+  # Generate the observed data 'z' using multivariate normal distribution
+  # We are setting the mean = 0 since REML circumvents estimating the mean
   
-  out1 <- nlm(likelihood.exponential_REML, cov.pars, stepmax=5, print.level=2, gradtol=10^(-10))
-
-  out2 <- (out1$est)**2
-
+  # Fit separate Matern models for each column of 'z' using REML
+  variogram_fits <- lapply(1:ncol(z), function(col) {
+    likfit(
+      geodata = list(coords = coords, data = z[, col]), # Use each column of 'z'
+      ini.cov.pars = c(1, 1), 
+      fix.nugget = TRUE, # Assuming a fixed nugget
+      lik.method = "REML" # Use REML estimation
+    )
+  })
+  
+  # Calculate the average values of the model parameters
+  avg_params <- sapply(variogram_fits, function(fit) {
+    cov.pars <- fit$cov.pars
+  })
+  
+  # Store the average values of the model parameters
+  out2 <- rowMeans(avg_params) # Average Model Parameters for Exponential Cov
+  
   sims2[i,] <- out2
-
+  
 }
 
+## 5. Obtain distribution, mean, and sd for each param estimate of the above simulation
 hist(sims2[,1])
 hist(sims2[,2])
 mean(sims2[,1]) - 4
